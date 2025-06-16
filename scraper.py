b3eb6a51343efb94fb83e7019c34b82b6c19d8d5
Kayalac <89374625+Kayalac@users.noreply.github.com>
2025-06-16 00:23:31 -0600

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,8 +6,12 @@ url = 'https://www.ficohsa.hn/'
 res = requests.get(url)
 soup = BeautifulSoup(res.text, 'html.parser')
 
-# Encuentra el span con el tipo de cambio
+# Encuentra el span con el tipo de cambio correcto
 span = soup.select_one('.gff-indicadores-divisas-v1__sale-value.value-one')
+
+if not span:
+    raise Exception("No se pudo encontrar el tipo de cambio. El selector puede haber cambiado.")
+
 rate = span.text.replace('L', '').strip().replace(',', '.')
 
 data = {
